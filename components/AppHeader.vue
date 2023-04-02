a4c6f8889a4d3025f298c52e2abb26a7080722fa
BuildTools <unconfigured@null.spigotmc.org>
2023-04-03 08:08:33 +0900

    AppheaderEdit

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -7,16 +7,11 @@
         <div class="flex-none">
             <div class="lg:hidden">
                 <div class="dropdown">
-                    <label tabindex="0" class="btn btn-ghost btn- swap swap-rotate">
-                        <input type="checkbox" />
-                        <svg class="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32"
-                            viewBox="0 0 512 512">
-                            <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
-                        </svg>
-                        <svg class="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32"
-                            viewBox="0 0 512 512">
-                            <polygon
-                                points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+                    <label tabindex="0" class="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
                         </svg>
                     </label>
                     <ul tabindex="0"
@@ -39,6 +34,7 @@
         </div>
     </div>
 </template>
+
 
 <!--
     <div>
